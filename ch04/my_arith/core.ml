82189f5ac37723651c40f496beaf4c198cb6dd49
yui-knk <spiketeika@gmail.com>
2018-03-11 18:50:24 +0900

    Practice 4.2.2

--- ch04/my_arith/core.ml
+++ ch04/my_arith/core.ml
@@ -2,10 +2,6 @@ open Syntax
 
 exception NoRuleApplies
 
-type result =
-    S of term
-  | E of term
-
 let rec isnumericval t =
   match t with
   | TmZero -> true
@@ -21,44 +17,40 @@ let rec isval t =
 
 let rec eval1 t =
   match t with
-  | TmIf(TmTrue, t2, t3) -> S(t2)
-  | TmIf(TmFalse, t2, t3) -> S(t3)
+  | t1 when isval t -> t1
   | TmIf(t1, t2, t3) ->
       (
-        let t' = eval1 t1 in
-        match t' with
-        | S(t1') -> S(TmIf(t1', t2, t3))
-        | E(_) -> E(TmIf(t1, t2, t3))
+        let e1 = eval1 t1 in
+        let e2 = eval1 t2 in
+        let e3 = eval1 t3 in
+        match (e1, e2, e3) with
+        | (TmTrue, t2', _) -> t2'
+        | (TmFalse, _, t3') -> t3'
+        | _ -> TmIf(t1, t2, t3)
       )
   | TmSucc(t1) ->
       (
-        let t' = eval1 t1 in
-        match t' with
-        | S(t1') -> S(TmSucc(t1'))
-        | E(_) -> E(TmSucc(t1))
+        let e1 = eval1 t1 in
+        match e1 with
+        | t1' when isnumericval t1' -> TmSucc(t1')
+        | _ -> TmSucc(t1)
       )
-  | TmPred(TmZero) -> S(TmTrue)
-  | TmPred(TmSucc(nv1)) when isnumericval nv1 -> S(nv1)
   | TmPred(t1) ->
       (
-        let t' = eval1 t1 in
-        match t' with
-        | S(t1') -> S(TmPred(t1'))
-        | E(_) -> E(TmPred(t1))
+        let e1 = eval1 t1 in
+        match e1 with
+        | TmZero -> TmZero
+        | TmSucc(t1') when isnumericval t1' -> t1'
+        | _ -> TmPred(t1)
       )
-  | TmIsZero(TmZero) -> S(TmTrue)
-  | TmIsZero(TmSucc(nv1)) when isnumericval nv1 -> S(TmFalse)
   | TmIsZero(t1) ->
       (
-        let t' = eval1 t1 in
-        match t' with
-        | S(t1') -> S(TmIsZero(t1'))
-        | E(_) -> E(TmIsZero(t1))
+        let e1 = eval1 t1 in
+        match e1 with
+        | TmZero -> TmTrue
+        | TmSucc(t1') when isnumericval t1' -> TmFalse
+        | _ -> TmIsZero(t1)
       )
-  | _ -> E(t)
+  | _ -> t
 
-let rec eval t =
-  let t' = eval1 t in
-  match t' with
-  | S(t2) -> eval t2
-  | E(t2) -> t2
+let rec eval t = eval1 t
