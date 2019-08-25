20caaba102fcca2f7749116ccc79c356786b48ad
yui-knk <spiketeika@gmail.com>
2019-08-25 16:28:24 +0900

    counterRep

--- ch18/counter.ml
+++ ch18/counter.ml
@@ -5,33 +5,20 @@ type counter = {
   inc: unit -> unit;
 };;
 
-type resetCounter = {
-  get: unit-> int;
-  inc: unit -> unit;
-  reset: unit -> unit;
+type counterRep = {
+  x: int ref;
 };;
 
 let newCounter =
-  fun u -> let x = ref 1 in
+  fun u -> let r = {x = ref 1} in
     {
-      get = (fun () -> !x);
-      inc = (fun () -> x := !x + 1);
+      get = (fun () -> !(r.x));
+      inc = (fun () -> r.x := !(r.x) + 1);
     };;
-
-let newResetCounter =
-  fun u -> let x = ref 1 in
-    {
-      get = (fun () -> !x);
-      inc = (fun () -> x := !x + 1);
-      reset = (fun () -> x := 1)
-    };;
-
-let inc3 c = c.inc (); c.inc (); c.inc ();;
 
 let c = newCounter ();;
-let rc = newResetCounter ();;
 
-(* inc3 c;; *)
-inc3 rc;;
+c.inc ();;
+c.inc ();;
 
-printf "%d : %d\n" (c.get ()) (rc.get ());;
+printf "%d\n" (c.get ());;
