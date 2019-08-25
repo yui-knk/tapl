bd71a3bdb0e487e9c78a0f44696d4da35729eae2
yui-knk <spiketeika@gmail.com>
2019-08-25 16:22:13 +0900

    inc3

--- ch18/counter.ml
+++ ch18/counter.ml
@@ -1,17 +1,37 @@
+open Printf
+
 type counter = {
   get: unit-> int;
   inc: unit -> unit;
 };;
 
+type resetCounter = {
+  get: unit-> int;
+  inc: unit -> unit;
+  reset: unit -> unit;
+};;
+
 let newCounter =
   fun u -> let x = ref 1 in
     {
-      get = (fun u -> !x);
-      inc = (fun u -> x := !x + 1);
+      get = (fun () -> !x);
+      inc = (fun () -> x := !x + 1);
     };;
 
-let c = newCounter ();;
+let newResetCounter =
+  fun u -> let x = ref 1 in
+    {
+      get = (fun () -> !x);
+      inc = (fun () -> x := !x + 1);
+      reset = (fun () -> x := 1)
+    };;
 
-c.inc ();;
-c.inc ();;
-print_int (c.get ());;
+let inc3 c = c.inc (); c.inc (); c.inc ();;
+
+let c = newCounter ();;
+let rc = newResetCounter ();;
+
+(* inc3 c;; *)
+inc3 rc;;
+
+printf "%d : %d\n" (c.get ()) (rc.get ());;
