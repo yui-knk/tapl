95209c9f3b413eeeb16fa4ffc4a59140d92cfe9b
yui-knk <spiketeika@gmail.com>
2019-10-27 15:55:50 +0900

    instrCounterClass
    But "parameter type mismatch" on counterClassRef.ml:37.19

--- ch18/counterClassRef.ml
+++ ch18/counterClassRef.ml
@@ -1,5 +1,13 @@
 CounterRep = {x: Ref Nat};
+InstrCounterRep = {x: Ref Nat, a: Ref Nat};
+
 SetCounter = {get: Unit -> Nat, set: Nat -> Unit, inc: Unit -> Unit};
+InstrCounter = {
+  get: Unit -> Nat,
+  set: Nat -> Unit,
+  inc: Unit -> Unit,
+  accesses: Unit -> Nat
+};
 
 setCounterClass =
   lambda r: CounterRep.
@@ -22,6 +30,17 @@ newSetCounter =
     let r = {x = ref 1} in
     let cAux = ref dummySetCounter in
     (cAux := setCounterClass r cAux; !cAux);
+
+instrCounterClass =
+  lambda r: InstrCounterRep.
+    lambda self: Ref InstrCounter.
+      let super = setCounterClass r self in
+        {
+          get = super.get,
+          set = super.set,
+          inc = super.inc,
+          accesses = lambda _: Unit. !(r.a)
+        };
 
 sc = newSetCounter unit;
 sc.inc unit;
